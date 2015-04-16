6e84256d8ee1c68b7bf7f8ec0c0d7a09c90627d6
Chris Anderson (WINDOWS) <Chris.Anderson@microsoft.com>
2015-04-16 10:28:43 -0700

    initial empty project

---
+++ KeyWatcher/Watcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyWatcher
+{
+    class DisposeToken : IDisposable 
+    {
+        internal DisposeToken() 
+        {
+            // capture state
+        }
+        ~DisposeToken() 
+        {
+            Dispose(false);
+        }
+        void Dispose(bool disposing) 
+        {
+            // stop watcher
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    public delegate void KeyStateChanged();
+
+    public static class Watcher
+    {
+        public static IDisposable Start(KeyStateChanged stateChanged)
+        {
+            // UNDONE
+            return new DisposeToken();
+        }
+    }
+}
